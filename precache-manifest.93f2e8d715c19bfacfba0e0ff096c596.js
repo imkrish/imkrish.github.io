62aa67bd507b3f4d3ded5e3eba0f417b40991810
Keerati Limkulphong <imkrish.developer@gmail.com>
2020-03-15 11:35:14 +1300

    Updates

---
+++ precache-manifest.93f2e8d715c19bfacfba0e0ff096c596.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "069527f8aaba66de829deea0a8199830",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e32f17a1ff409e24ff14",
+    "url": "/static/css/main.9009c3c7.chunk.css"
+  },
+  {
+    "revision": "1c3c1dbbbc3eb5891dc9",
+    "url": "/static/js/2.d0f78141.chunk.js"
+  },
+  {
+    "revision": "b8bc74e4984c0e83b77b30f19f102633",
+    "url": "/static/js/2.d0f78141.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e32f17a1ff409e24ff14",
+    "url": "/static/js/main.31e73fe7.chunk.js"
+  },
+  {
+    "revision": "68df8aa5a26769db4de3",
+    "url": "/static/js/runtime-main.201b5c3e.js"
+  }
+]);
